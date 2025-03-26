4ae6e21cd96f823f70f39acea9c44bfb222a21f1
Frusadev <frusadev@gmail.com>
2025-03-26 08:52:42 +0000

    Fix metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,27 @@
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "./components/ThemeProvider";
 import "./globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Linux and Open-Source Lovers",
+  icons: "/favicon.png",
+  description: "A community of Linux and open-source lovers",
+  openGraph: {
+    title: "Linux and Open-Source lovers",
+    description: "A community of Linux and open-source lovers",
+    url: "https://loslc.netlify.app",
+    siteName: "LOSLC",
+    images: [
+      {
+        url: "/loslcb.png",
+        width: 1200,
+        height: 825,
+      }
+    ],
+    type: "website",
+  }
+}
 
 export default function RootLayout({
   children,
